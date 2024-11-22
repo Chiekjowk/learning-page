83e5837c5e2b64724cf5dda5d0e4d8bb04cbc391
chiekjowk <ikmiles83@gmail.com>
2024-11-22 22:53:34 +0200

    did changes on the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Header from './components/Header/Header.jsx'
+import Main from './components/Main/Main.jsx'
 const App = () => {
   return (
     <div>
-      
+      <Header />
+      <Main />
     </div>
   )
 }
---
+++ src/info.js
@@ -0,0 +1,27 @@
+export const info = [
+    {
+        "id": 1,
+        "img":"src/assets/pexels-mikhail-nilov-7988079.jpg",
+        "des":"Learn how to make a website",
+        "profile":"src/assets/img1.jpg",
+        "name":"William Clark",
+        "price":"$89.99"
+    },
+    {
+        "id": 1,
+        "img":"src/assets/pexels-divinetechygirl-1181259.jpg",
+        "des":"Learn digital marketing for beginners",
+        "profile":"src/assets/img1.jpg",
+        "name":"Charlotte Grace",
+        "price":"$79.99"
+    },
+    {
+        "id": 1,
+        "img":"src/assets/pexels-naboth-otieno-83498565-19805876.jpg",
+        "des":"Learn advanced programming",
+        "profile":"src/assets/img1.jpg",
+        "name":"Logan Dusk",
+        "price":"$99.99"
+    }
+
+]
