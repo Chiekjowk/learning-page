b6de0c9a7789189b4165eb841b7545b1f78ee071
Felix Wani <lokosangwani21@Gmail.com>
2024-11-22 23:36:41 +0200

    added the images to the folder

--- src/component/section4/Section4.jsx
+++ src/component/section4/Section4.jsx
@@ -1,22 +1,23 @@
+import lady from "../../assets/Office_lady.png";
+import gent from "../../assets/Office_gentleman.png";
+
 import "./Section4.css";
 export default function Section4() {
   return (
     <>
       {/* Best Education Platform */}
-      <section className="grid grid-cols-2 gap-12 p-12">
+      <section className="section4-1 grid grid-cols-2 gap-12 p-12">
+        <img src={lady} alt="Teacher" className="ladyImg" />
         <div className="flex flex-col justify-center">
           <h2 className="mb-4 text-3xl font-bold">Best Education platform</h2>
           <p className="mb-6 text-gray-600">
             Looking for top quality education? We offer a diverse range of
             courses, expert instructors, and flexible learning options.
           </p>
-          <button className="self-start px-6 py-3 bg-yellow-400 rounded-md">
-            Read more
-          </button>
+          <button className="primary-btn">Read more</button>
         </div>
-        <img src="" alt="Teacher" className="rounded-lg" />
       </section>
-      <section className="grid grid-cols-2 gap-12 p-12">
+      <section className="section4-1 grid grid-cols-2 gap-12 p-12">
         <div className="relative">
           <div className="flex flex-col justify-center">
             <h2 className="mb-4 text-3xl font-bold">Best Education platform</h2>
@@ -24,11 +25,9 @@ export default function Section4() {
               Looking for top quality education? We offer a diverse range of
               courses, expert instructors, and flexible learning options.
             </p>
-            <button className="self-start px-6 py-3 bg-yellow-400 rounded-md">
-              Read more
-            </button>
+            <button className="primary-btn">Read more</button>
           </div>
-          <img src="" alt="Teacher" className="rounded-lg" />
+          <img src={gent} alt="Teacher" className="gentImg" />
         </div>
       </section>
     </>
